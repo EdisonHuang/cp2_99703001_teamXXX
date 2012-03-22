fa95931d370a297e3b5ad09becd2fb559ae7d94b
EdisonHuang <99703001@nccu.edu.tw>
2012-03-22 19:54:30 +0800

    added example.js

---
+++ example.js
@@ -0,0 +1,21 @@
+# comments
+# teamXX
+# 99XXXXXX, 99XXXXXX, 99XXXXXX
+
+function hello1() {
+print(“hello, member1’s name”);
+}
+
+function hello2() {
+print(“hello, member2’s name”);
+}
+
+function hello3() {
+  print(“hello, member2’s name”);
+}
+
+function main() {
+  hello1();
+  hello2();
+  hello3();
+}
